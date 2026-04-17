03a82c1340274f13174cc47ce719014eec29574d
collab-rishi <convey.rishi@gmail.com>
2026-04-17 21:40:53 +0530

    chore: remove unused boilerplate and lean down dependencies

--- src/routes/index.routes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { hello } from '../controllers/index.controller';
-
-const router = express.Router();
-
-router.route('/').get(hello);
-
-export default router;
